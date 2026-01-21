14a70bb4f668c235694a33812423fedbda6f99bd
Thiago <thiagocattelam123@gmail.com>
2026-01-20 23:00:30 -0300

    adicionado `lint:check`, `lint:write` e dependencias do prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home () {
-    return <h1>Obrigado GLS (gilson TI SUPPORT)</h1>
+function Home() {
+  return <h1>Obrigado GLS (gilson TI SUPPORT)</h1>;
+}
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
